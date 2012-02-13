bbc58fce639ec879c8b03f548150eb7e6d5854b2
Riccardo Causo <rixlabs@gmail.com>
2012-02-13 15:54:06 +0100

    register logic

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,7 +4,8 @@ class AccountsController < ApplicationController
   # GET /accounts
   # GET /accounts.json
   def index
-    @accounts = current_user.accounts
+    @accounts = Account.all
+	@accounts = @accounts - current_user.accounts
 	
     respond_to do |format|
       format.html # index.html.erb
@@ -83,8 +84,24 @@ class AccountsController < ApplicationController
     @account.destroy
 
     respond_to do |format|
-      format.html { redirect_to user_accounts_path(@account.user) }
+      format.html { redirect_to accounts_path }
       format.json { head :ok }
     end
   end
+  
+  # POST /accounts/id/resgister
+  # register an account for the watch list
+  
+  def register
+	@account = Account.find(params[:id])
+	
+	@account.users << current_user
+	
+	 respond_to do |format|
+      format.html { redirect_to dashboard_index_path }
+      format.json { head :ok }
+    end
+	
+  end
+  
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
 	user = User.find_by_username(params[:username])
 	if user && user.authenticate(params[:password])
 		session[:user_id] = user.id
-		redir_path = tests_path
-		redir_path = session[:return_to] if session[:return_to]
+		redir_path = dashboard_index_path
+		#redir_path = session[:return_to] if session[:return_to]
 		redirect_to redir_path, :notice => "Logged in!"
 	else
 		flash.now.alert = "Invalid email or password"
